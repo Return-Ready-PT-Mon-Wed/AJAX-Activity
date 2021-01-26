79e256c7282aca263181be1d1aa09c849565ecb2
AntsmithCode <hey@antsmith.dev>
2021-01-26 07:44:05 -0500

    (App) Switched from appendchild DOM Elements to template literal

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const ApiHttps = "https://dog.ceo/api/breeds/image/random/3";
+const btn = document.querySelector('.btn');
+const randomPupElement = document.querySelector('.card-group');
 
 async function getRandomPup() {
-  randomPups.innerHTML = "";
-  const response = await fetch(ApiHttps); // resolve promise
-  const json = await response.json(); // parse into json
-  console.log(json.message);
-}
+    randomPupElement.innerHTML = "";
+    const response = await fetch(ApiHttps); // resolve promise
+    const json = await response.json(); // parse into json
+    console.log(json.message);
 
-getRandomPup();
+    json.message.foreach(bitcoinPupImage => {
+        randomPupElement.innerHTML += `
+        <div class="card">
+            <img src="${bitcoinPupImage}" class="card-img-top">
+                <div class="card-body">
+        </div>
+                </div>
+
+        `;
+        //  function generateCards(json) {
+        //    const container = document.createElement("container");
+        //    randomPupList.appendChild(container); // create template literal
+        //    container.innerHTML = `
+
+        //      `;
+        //  }
+    }),
+
+    btn.addEventListener('click', getRandomPup);
