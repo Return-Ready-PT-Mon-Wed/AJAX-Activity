4a06ad63f2b43c5423984acd7a15b6c054f70064
Andre Logan <andrelogan@andres-air.myfiosgateway.com>
2021-01-26 08:30:19 -0500

    my first ajax

---
+++ ajax.js
@@ -0,0 +1,14 @@
+function loadDoc() {
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+       // document.getElementById("dogpic").innerHTML = this.responseText;
+       var img = JSON.parse(xhr.responseText);
+       document.getElementById("dogpic").src=img.message;
+       document.getElementById("dogpic2").src=img.message;
+       document.getElementById("dogpic3").src=img.message;
+    }
+};
+xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+xhr.send();
+}
