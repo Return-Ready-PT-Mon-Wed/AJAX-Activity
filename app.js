58597f4de8d7250765c6e83c4afb6f616233d140
Anthony Smith <phreedev@Anthonys-MacBook-Air.local>
2021-01-26 12:51:58 -0500

    (APP) Stable v1

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 const ApiHttps = "https://dog.ceo/api/breeds/image/random/3";
-const btn = document.querySelector('.btn');
-const randomPupElement = document.querySelector('.card-group');
+const btn = document.querySelector(".btn");
+const randomPupElement = document.querySelector(".card-group");
 
 async function getRandomPup() {
-    randomPupElement.innerHTML = "";
-    const response = await fetch(ApiHttps); // resolve promise
-    const json = await response.json(); // parse into json
-    console.log(json.message);
+  const response = await fetch(ApiHttps); // resolve promise
+  const json = await response.json(); // parse into json
+  console.log(json.message);
 
-    json.message.foreach(bitcoinPupImage => {
-        randomPupElement.innerHTML += `
+  // foreach iterates over the message array
+  json.message.forEach((bitcoinPupImage) => {
+    // create template literal for Dom Elements
+    randomPupElement.innerHTML += `
         <div class="card">
-            <img src="${bitcoinPupImage}" class="card-img-top">
-                <div class="card-body">
+            <div class ="card-img-top">
+                <img src="${bitcoinPupImage}">
+                    <div class="card-body">
+                    </div>
+            </div>
         </div>
-                </div>
-
         `;
-        //  function generateCards(json) {
-        //    const container = document.createElement("container");
-        //    randomPupList.appendChild(container); // create template literal
-        //    container.innerHTML = `
 
-        //      `;
-        //  }
-    }),
+    //  function generateCards(json) {
+    //    const container = document.createElement("container");
+    //    randomPupList.appendChild(container); // create template literal
+    //    container.innerHTML = `
 
-    btn.addEventListener('click', getRandomPup);
+    //      `;
+    //  }
+  });
+}
+//button activates the function
+btn.addEventListener("click", getRandomPup);
