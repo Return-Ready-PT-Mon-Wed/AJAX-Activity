227a458583c0dfb724e3dd92a382ab008af2d463
Amatullah Brown <amatullahbrown@Amatullahs-Air.fios-router.home>
2021-01-25 21:01:38 -0500

    completed using vanilla js and jquery

--- dog.js
+++ dog.js
@@ -1,21 +1,23 @@
-const xhr = new XMLHttpRequest();
-
+//completed using vanilla js
 function createImg() {
+    const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
             const response = JSON.parse(xhr.responseText);
-            const img = `<img src="${response.message}">`;
-            document.getElementById('dog-div').innerHTML += img;
+            let img;
+            for (let i=0; i<3; i++) {
+                img = `<img src="${response.message[i]}">`;
+                document.getElementById('dog-div').innerHTML = img + document.getElementById('dog-div').innerHTML;
+            }
         }
-        
+        console.log(xhr);
+        console.log(response.message);
     };
     
-    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random/3');
     xhr.send();
 }
-const revealBtn = document.getElementById('revealBtn');
+
 function sendAJAX() {
-    createImg();
-    createImg();
     createImg();
 }
