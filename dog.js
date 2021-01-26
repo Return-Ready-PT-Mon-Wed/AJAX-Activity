77690ce3f02998bdf96088563e4421ec56bc6737
Amatullah Brown <amatullahbrown@Amatullahs-Air.fios-router.home>
2021-01-25 19:36:51 -0500

    This has the button produce a new image each time

--- dog.js
+++ dog.js
@@ -1,16 +1,21 @@
 const xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-        const response = JSON.parse(xhr.responseText);
-        const img = `<img src="${response.message}">`;
-        document.getElementById('dog-div').innerHTML = img;
-    }
+function createImg() {
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            const response = JSON.parse(xhr.responseText);
+            const img = `<img src="${response.message}">`;
+            document.getElementById('dog-div').innerHTML += img;
+        }
+        
+    };
     
-};
-
-xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    xhr.send();
+}
 const revealBtn = document.getElementById('revealBtn');
 function sendAJAX() {
-    xhr.send();
+    createImg();
+    createImg();
+    createImg();
 }
