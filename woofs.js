641e2d1e517adb38290a0a2c0bbb391ac0b34ae8
JustinHaase <74632537+JustinHaase@users.noreply.github.com>
2021-01-28 17:50:55 -0500

    completed lab

---
+++ woofs.js
@@ -0,0 +1,39 @@
+const dogLink = 'https://dog.ceo/api/breeds/image/random/3';
+
+let button = document.querySelector('button');
+
+const image = document.getElementById('dogImage1')
+const image1 = document.getElementById('dogImage2')
+const image2 = document.getElementById('dogImage3')
+
+const docs = document.querySelector('#woofs');
+
+button.addEventListener('click', sendAJAX);
+
+function play(){
+    var audio = new Audio('media/sound1.mp3');
+    audio.play();
+}
+
+function sendAJAX() {
+    fetch(dogLink).then(res => res.json()).then(({message})=>{
+      image.innerHTML = `<img src=${message[0]}>`;
+      image1.innerHTML = `<img src=${message[1]}>`;
+      image2.innerHTML = `<img src=${message[2]}>`;
+  }
+ )
+};
+
+// xhr.open("GET", "https://dog.ceo/api/breeds/image/random/3");
+
+// function sendAJAX() {
+//   xhr.send();
+// }
+
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       var n = JSON.parse(xhr.responseText);
+//       var sec = JSON.parse(xhr.responseText);
