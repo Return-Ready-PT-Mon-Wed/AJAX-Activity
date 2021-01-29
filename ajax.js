df02679cc172ceefdb9eff9a7f64dc03e258f8d6
Kri'Shawn Brown <krishawn.brown@gmail.com>
2021-01-28 22:00:23 -0500

    ajax edits

--- ajax.js
+++ ajax.js
@@ -3,22 +3,25 @@ function dog() {
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
-            var data = JSON.parse(xhr.responseText);
-            document.getElementById("dogs").src = data.message;
-            document.getElementById("dogs").alt = 'Random dog pics';
+            let image = JSON.parse(xhr.responseText);
             console.log(xhr.responseText);
+            document.getElementById("dogs1").src = image.message[0];
+            document.getElementById("dogs2").src = image.message[1];
+            document.getElementById("dogs3").src = image.message[2];
 
         }
     }
-    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random');
+    xhr.open('GET', 'https://dog.ceo/api/breeds/image/random/3');
     xhr.send();
 
 }
 
-function myFunction() {
-    document.getElementById('btn').addEventListener("click", myFunction());
-}
+//function myFunction() {
+//document.getElementById('btn').addEventListener("click", myFunction());
+//}
 
-function myFunction() {
-    document.getElementById("dogs").src = "hackanm.gif";
-}
+//function myFunction() {
+//document.getElementById("dogs").src = "hackanm.gif";
+//}
+//document.getElementById("dogs").src = data.message;
+//document.getElementById("dogs").alt = 'Random dog pics';
